86abfdaa2a082f41e50441278fd0dd0069184717
hruhru042-dev <hruhru042@gmail.com>
2025-09-16 10:11:34 +0300

    Add files via upload

---
+++ semester_1/lab2_integer_arithmetics/lab2_ex4_1/lab2_ex4_1/lab2_ex4_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main() {
+    int n;
+    std::cout << "enter n:\n";
+    std::cin >> n;
+
+    std::cout << "perfect numbers <= n " << n << ": ";
+    for (int i = 2; i <= n; ++i) {
+        int sum = 0;
+        for (int t = 1; t <= i / 2; ++t) {
+            if (i % t == 0) {
+                sum += t;
+            }
+        }
+        if (i == sum) {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << std::endl;
+    return 0;
+}
+

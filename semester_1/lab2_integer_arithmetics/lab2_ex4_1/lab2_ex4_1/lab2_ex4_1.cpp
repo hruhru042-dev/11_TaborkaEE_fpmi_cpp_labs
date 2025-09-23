07e71280da8f711a76218684197d39c2efee762c
Taborka Elena <hruhru042@gmail.com>
2025-09-23 09:09:51 +0300

    ex4

--- semester_1/lab2_integer_arithmetics/lab2_ex4_1/lab2_ex4_1/lab2_ex4_1.cpp
+++ semester_1/lab2_integer_arithmetics/lab2_ex4_1/lab2_ex4_1/lab2_ex4_1.cpp
@@ -4,19 +4,29 @@ int main() {
     int n;
     std::cout << "enter n:\n";
     std::cin >> n;
-
-    std::cout << "perfect numbers <= n = " << n << ": ";
-    for (int i = 2; i <= n; ++i) {
-        int sum = 0;
-        for (int t = 1; t <= i / 2; ++t) {
-            if (i % t == 0) {
-                sum += t;
-            }
+    while (!(std::cin) || (n <= 0)) {
+        if (!(std::cin)) {
+            std::cout << "Error" << std::endl;
+            break;
         }
-        if (i == sum) {
-            std::cout << i << " ";
-        }
+        std::cout << "enter an int n greater than zero: ";
+        std::cin >> n;
     }
+            std::cout << "perfect numbers <= n = " << n << ": ";
+            for (int i = 2; i <= n; ++i) {
+                int sum = 0;
+                for (int t = 1; t <= i / 2; ++t) {
+                    if (i % t == 0) {
+                        sum += t;
+                    }
+                }
+                if (i == sum) {
+                    std::cout << i << " ";
+                }
+            }
+        
+
+    
     return 0;
 }
 

7259352c36821a1241521998e241767f08a3a546
hruhru042-dev <hruhru042@gmail.com>
2025-09-30 06:31:26 +0300

    Add files via upload

---
+++ semester_1/lab3_pointers_and_arrays/lab3_var19_fin/lab3_var19_fin/lab3_var19_fin.cpp
@@ -0,0 +1,111 @@
+﻿
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <set>
+#include <cmath>
+const int length = 1000;
+int main(){
+    int n;
+    float arr[length];  
+    std::cout << "Enter size of array: ";
+    std::cin >> n;
+    if (n > length) {
+        std::cout << "Size too large!" << std::endl;
+        return 1;
+    }
+    bool choice;
+    std::cout << "How do you want to enter the array: randomly (enter 1) or by yourself (enter 0)? ";
+    if (!(std::cin >> choice)) {
+        std::cout << "Error, enter a number" << std::endl;
+        return 1;
+    }
+    if (choice == 1) {
+        srand(static_cast<unsigned>(time(0)));
+        float min_val, max_val;
+        std::cout << "Enter minimum range limit: ";
+        std::cin >> min_val;
+        std::cout << "Enter maximum range limit: ";
+        std::cin >> max_val;
+        if (max_val < min_val) {
+            float temp = max_val;
+            max_val = min_val;
+            min_val = temp;
+        }
+        std::random_device rd;
+        std::mt19937 gen(45218965);
+        std::uniform_real_distribution<double>
+            dist(min_val, max_val);
+        for (int i = 0; i < n; ++i) {
+            arr[i] = dist(gen);
+        }
+        std::cout << "Your array: ";
+        for (int i = 0; i < n; ++i) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    else {
+        for (int i = 0; i < n; ++i) {
+            std::cout << "Element " << i + 1 << ": ";
+            std::cin >> arr[i];
+            if (!(std::cin)) {
+                std::cout << "Error, enter a number" << std::endl;
+                break;
+            }
+        }
+        std::cout << "Your array: ";
+        for (int i = 0; i < n; ++i) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::set<float> unique;
+    for (int i = 0; i < n; ++i) {
+        unique.insert(arr[i]);
+    }
+    std::cout << "The number of different elements: " << unique.size() << std::endl;
+    int first_negative_idx = -1;
+    int last_negative_idx = -1;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] < 0) {
+            first_negative_idx = i;
+            break;
+        }
+    }
+    for (int i = n - 1; i >= 0; --i) {
+        if (arr[i] < 0) {
+            last_negative_idx = i;
+            break;
+        }
+    }
+    if (first_negative_idx != -1 && last_negative_idx != -1 && first_negative_idx < last_negative_idx) {
+        float sum = 0;
+        for (int i = first_negative_idx + 1; i < last_negative_idx; ++i) {
+            sum += arr[i];
+        }
+        std::cout << "The sum of the elements between the first and last negative: " << sum << std::endl;
+    }
+    else {
+        std::cout << "No negative elements found or there is only one";
+    }
+    int x;
+    std::cout << "Enter number X ";
+    std::cin >> x;
+    int write_index = 0;
+    for (int read_index = 0; read_index < n; ++read_index) {
+        if (std::fabs(arr[read_index]) <= x) {
+            arr[write_index++] = arr[read_index];
+        }
+    }
+    for (int i = write_index; i < n; ++i) {
+        arr[i] = 0;
+    }
+    std::cout << "Compressed array: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
---
+++ semester_1/lab3_pointers_and_arrays/lab3_var37/lab3_var37/lab3_var37.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <random>
+#include <ctime>
+#include <cmath>
+void mySwap(float& a, float& b) {
+    float temp = a;
+    a = b;
+    b = temp;
+}
+int main() {
+    int n;
+    std::cout << "Enter size of array: ";
+    std::cin >> n;
+    if (n <= 0) {
+        std::cout << "Invalid size!" << std::endl;
+        return 1;
+    }
+    float* arr = new float[n];
+    bool choice;
+    std::cout << "How do you want to enter the array: randomly (enter 1) or by yourself (enter 0)? ";
+    if (!(std::cin >> choice)) {
+        std::cout << "Error, enter a number" << std::endl;
+        delete[] arr;
+        return 1;
+    }
+    if (choice == 1) {
+        srand(static_cast<unsigned>(time(0)));
+        float min_val, max_val;
+        std::cout << "Enter minimum range limit: ";
+        std::cin >> min_val;
+        std::cout << "Enter maximum range limit: ";
+        std::cin >> max_val;
+        if (max_val < min_val) {
+            float temp = max_val;
+            max_val = min_val;
+            min_val = temp;
+        }
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<float> dist(min_val, max_val);
+        for (int i = 0; i < n; ++i) {
+            arr[i] = dist(gen);
+        }
+    }
+    else {
+        for (int i = 0; i < n; ++i) {
+            std::cout << "Element " << i + 1 << ": ";
+            std::cin >> arr[i];
+            if (!std::cin) {
+                std::cout << "Error, enter a number" << std::endl;
+                delete[] arr;
+                return 1;
+            }
+        }
+    }
+    std::cout << "Your array: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    int maxLength = 1; 
+    int currentLength = 1; 
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] >= arr[i - 1]) {
+            ++currentLength; 
+        }
+        else {
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+            }
+            currentLength = 1;
+        }
+    }
+    if (currentLength > maxLength) {
+        maxLength = currentLength;
+    }
+    std::cout << "Length of the longest ordered chain: " << maxLength << std::endl;
+    int maxIdx = 0;
+    int minIdx = 0;
+    for (int i = 1; i < n; ++i) {
+        if (fabs(arr[i]) > fabs(arr[maxIdx])) {
+            maxIdx = i;
+        }
+        if (fabs(arr[i]) < fabs(arr[minIdx])) {
+            minIdx = i;
+        }
+    }
+    int start = (maxIdx < minIdx) ? maxIdx : minIdx;
+    int end = (maxIdx > minIdx) ? maxIdx : minIdx;
+    float product = 1.0;
+    if (start + 1 >= end) {
+        std::cout << "No elements between max and min by absolute value." << std::endl;
+    }
+    else {
+        for (int i = start + 1; i < end; ++i) {
+            product *= arr[i];
+        }
+        std::cout << "The product of elements between the maximum and minimum in absolute value: " << product << std::endl;
+    }
+    for (int i = 0; i < n; i += 2) {
+        for (int j = i + 2; j < n; j += 2) {
+            if (arr[i] < arr[j]) {
+                mySwap(arr[i], arr[j]);
+            }
+        }
+    }
+    for (int i = 1; i < n; i += 2) {
+        for (int j = i + 2; j < n; j += 2) {
+            if (arr[i] > arr[j]) {
+                mySwap(arr[i], arr[j]);
+            }
+        }
+    }
+    std::cout << "New array: ";
+    for (int i = 0; i < n; i++)
+        std::cout << arr[i] << " ";
+    delete[] arr;
+    return 0;
+}

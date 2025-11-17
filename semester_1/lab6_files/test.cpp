daca397bb2ee641eadb6ae9e08acece0e35767b3
hruhru042-dev <hruhru042@gmail.com>
2025-11-17 21:26:11 +0300

    Add files via upload

---
+++ semester_1/lab6_files/test.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include <fstream>
+void writeAvgToOutputFile(const std::string& filename, double value) {
+    std::ofstream out{ filename };
+    if (!(out.is_open())) {
+        throw "file cannot be open ";
+    }
+    out << value;
+    out.close();
+}
+int main() {
+    try {
+        writeAvgToOutputFile("output.txt", 5677);
+        std::cout << "file was create";
+    }
+    catch (const char* msg) {
+        std::cout << msg;
+    }
+}

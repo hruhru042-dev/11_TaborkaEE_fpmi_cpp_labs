b492e3c9c942564a9ddb58aaabab4b99504ffed5
hruhru042-dev <hruhru042@gmail.com>
2025-12-01 21:31:25 +0300

    Update vector_impl.h

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,24 @@
 #pragma once
-
+#include <cstddef>
+#include <initializer_list>
 class Vector {
-    // your code goes here
+
+private: 
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+    int* data_ = nullptr;
+public:
+    Vector();
+    Vector(size_t n);
+    Vector(std::initializer_list<int>);
+    Vector& operator=(const Vector& other);
+    Vector(const Vector& other);
+   ~Vector();
+    void Swap(Vector& other);
+    int& operator[](size_t index);
+    const int& operator[](size_t index) const;
+
+
+
 };
+
